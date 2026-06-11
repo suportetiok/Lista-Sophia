be6a44a9fdafba6909d04922aad12d574d4b37fb
suportetiok <suportetiok@gmail.com>
2026-06-11 01:07:03 -0400

    Update firebase.js
    "lista-sophia.firebaseapp.com",

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopu
 
 // SUA CONFIGURAÇÃO ORIGINAL
 const firebaseConfig = {
-    apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
-    authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
-    databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
-    projectId: "lista-de-presentes-33c7f",
-    storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
-    messagingSenderId: "540439045540",
-    appId: "1:540439045540:web:23587a2640c46784ab0d31"
+  apiKey: "AIzaSyCdwDnwBSRFB6oY2mg7qsB9OLF2csw5hag",
+  authDomain: "lista-sophia.firebaseapp.com",
+  databaseURL: "https://lista-sophia-default-rtdb.firebaseio.com",
+  projectId: "lista-sophia",
+  storageBucket: "lista-sophia.firebasestorage.app",
+  messagingSenderId: "10075688372",
+  appId: "1:10075688372:web:c33674f2f8912e931376e1"
 };
 
 const app = initializeApp(firebaseConfig);
